e72dd24eb1ff16263d78c93bb97316a0e0ccf294
Anastasiya <atassmorodina@gmail.com>
2023-01-25 06:00:29 +0300

    Первая задача done

---
+++ HW01/Program.cs
@@ -0,0 +1,82 @@
+﻿/*
+Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+3, 5 -> 243 (3⁵)
+
+2, 4 -> 16
+
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+
+6, 1, 33 -> [6, 1, 33]
+*/
+
+
+int GetNumber1(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+int GetNumber2(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 1)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число. Повторите ввод");
+        }
+    }
+
+    return result;
+}
+
+
+
+int GetDegree(int numberA, int numberB)
+{
+    int degree = 0;
+
+    for (int i = 0; i <= numberA && i <= numberB; i++)
+    {
+        degree = Convert.ToInt32(Math.Pow(numberA, numberB));
+    }
+
+    return degree;
+}
+
+int numberA = GetNumber1("Введите число больше 1");
+int numberB = GetNumber2("Введите число больше 1");
+int degree = GetDegree(numberA, numberB);
+Console.WriteLine($"Число {numberA} в степени {numberB} = {degree}");
