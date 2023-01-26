457b1fd427489fd3d6e3487e2a40f3d1e8af0382
Anastasiya <atassmorodina@gmail.com>
2023-01-26 05:16:02 +0300

    Почистила код

--- HW01/Program.cs
+++ HW01/Program.cs
@@ -4,21 +4,8 @@
 3, 5 -> 243 (3⁵)
 
 2, 4 -> 16
-
-Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
-452 -> 11
-
-82 -> 10
-
-9012 -> 12
-
-Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
-
-1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
-
-6, 1, 33 -> [6, 1, 33]
 */
+
 
 
 int GetNumber1(string message)
@@ -41,42 +28,18 @@ int GetNumber1(string message)
 
     return result;
 }
-int GetNumber2(string message)
-{
-    int result = 0;
-
-    while(true)
-    {
-        Console.WriteLine(message);
-
-        if(int.TryParse(Console.ReadLine(), out result) && result > 1)
-        {
-            break;
-        }
-        else
-        {
-            Console.WriteLine("Ввели не число. Повторите ввод");
-        }
-    }
-
-    return result;
-}
-
-
 
 int GetDegree(int numberA, int numberB)
 {
     int degree = 0;
-
     for (int i = 0; i <= numberA && i <= numberB; i++)
     {
         degree = Convert.ToInt32(Math.Pow(numberA, numberB));
     }
-
     return degree;
 }
 
 int numberA = GetNumber1("Введите число больше 1");
-int numberB = GetNumber2("Введите число больше 1");
+int numberB = GetNumber1("Введите число больше 1");
 int degree = GetDegree(numberA, numberB);
 Console.WriteLine($"Число {numberA} в степени {numberB} = {degree}");
